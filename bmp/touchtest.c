8f3bdec21d729d189a7d19815ae43de198b3be30
Jisub-Son <lttileson@naver.com>
2021-12-14 23:02:09 +0900

    터치 테스트 끝

---
+++ bmp/touchtest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include "touch.h"
+
+int main(void)
+{
+    touchInit();
+
+    int msgID = msgget(MESSAGE_ID, IPC_CREAT|0666);
+    TOUCH_MSG_T rcvMsg;
+
+    while(1)
+    {
+        msgrcv(msgID, &rcvMsg, sizeof(rcvMsg)-sizeof(long int), 0, 0);
+
+        switch (rcvMsg.keyInput)
+        {
+        case 999:
+            printf("x : %d/t y : %d\r\n", rcvMsg.x, rcvMsg.y);
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
